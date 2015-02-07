c1408725a3035c9b712ed0acbb884152f7dd09a2
Sir-Arturio <arto.iijalainen@gmail.com>
2015-02-07 10:12:41 +0200

    Added the current/base version of the fetch.sh script.

---
+++ fetch.sh
@@ -0,0 +1,1 @@
+wget -r -l inf -A .mp3 -erobots=off --no-parent --no-clobber --no-directories -H --domains server1.radiohelsinki.fi,www.radiohelsinki.fi https://www.radiohelsinki.fi/podcastit/Tukevasti+ilmassa
