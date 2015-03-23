acd14d8e1e17d09e7aa55d1844ff2be02e5f29e7
Sir-Arturio <arto.iijalainen@gmail.com>
2015-03-23 17:26:47 +0200

    Wrapped wiki code into a function.

--- wiki.php
+++ wiki.php
@@ -1,19 +1,29 @@
 <?php
 
-$content = file_get_contents('wiki2.txt');
+function read_wiki_content() {
+  $content = file_get_contents('wiki2.txt');
 
-$content = str_replace(array("[[", "]]", "&nbsp;"), '', $content);
-$content = preg_replace('/!\|/m', '||', $content);
-$matches = preg_split('/\|-|{\|/', $content);
+  $content = str_replace(array("[[", "]]", "&nbsp;"), '', $content);
+  $content = preg_replace('/!\|/m', '||', $content);
+  $matches = preg_split('/\|-|{\|/', $content);
 
-foreach($matches as &$row) {
-  $cells = explode("\n", $row);
-  $cells = array_filter($cells, function ($cell) { return preg_match('/\|\|/', $cell); });
-  $cells = preg_replace('/\|\|\s*/', '', $cells);
-  if(isset($cells[1]) && count($cells) >= 3) {
-    $cells[1] = prepare_date($cells[1]);
+  foreach($matches as &$row) {
+    $cells = explode("\n", $row);
+    $cells = array_filter($cells, function ($cell) { return preg_match('/\|\|/', $cell); });
+    $cells = preg_replace('/\|\|\s*/', '', $cells);
+    if(isset($cells[1]) && count($cells) >= 3) {
+      $cells[1] = prepare_date($cells[1]);
+    }
+    $row = $cells;
   }
-  $row = $cells;
+
+  $matches = array_filter($matches);
+  $matches = array_filter($matches, function($a) { return (is_object($a[1])) ? true : false; });
+  $matches = array_filter($matches, function($a) { return ($a[2] == 'Uusinta edellisestä') ? false : true; });
+  usort($matches, 'sort_by_date');
+  $matches = array_map(function($a) { $a[1] = flatten_date($a[1]); return $a; }, $matches);
+  print_r($matches);
+  return $matches;
 }
 
 function sort_by_date($a, $b) {
@@ -25,13 +35,6 @@ function sort_by_date($a, $b) {
 function flatten_date(DateTime $date) {
   return $date->format("Y-m-d");
 }
-
-$matches = array_filter($matches);
-$matches = array_filter($matches, function($a) { return (is_object($a[1])) ? true : false; });
-$matches = array_filter($matches, function($a) { return ($a[2] == 'Uusinta edellisestä') ? false : true; });
-usort($matches, 'sort_by_date');
-$matches = array_map(function($a) { $a[1] = flatten_date($a[1]); return $a; }, $matches);
-print_r($matches);
 
 /**
  * @param $finnish_date string
