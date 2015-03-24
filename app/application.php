abb511d18ff9b06842c77ecb8f14983dbd910669
Sir-Arturio <arto.iijalainen@gmail.com>
2015-03-24 22:29:29 +0200

    Created a stub for Run command. Also added src directory into composer.json as a base for PSR-4 autoloading.

--- app/application.php
+++ app/application.php
@@ -4,6 +4,8 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
+use TukevastiIlmassaDataCombiner\Console\Command\RunCommand;
 
 $application = new Application("Tukevasti Ilmassa Data Combiner");
+$application->add(new RunCommand);
 $application->run();
---
+++ src/Console/Command/RunCommand.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace TukevastiIlmassaDataCombiner\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class RunCommand extends Command
+{
+  protected function configure() {
+    $this->setName("run");
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    $output->writeln("test");
+  }
+}
