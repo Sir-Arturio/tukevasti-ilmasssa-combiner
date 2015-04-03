8cf3bb4a6c91f9ea10ed1d44702d36337862a739
Sir-Arturio <arto.iijalainen@gmail.com>
2015-04-03 13:27:17 +0300

    Converted wiki.php to a proper class and hooked it up to the RunCommand.

--- src/Console/Command/RunCommand.php
+++ src/Console/Command/RunCommand.php
@@ -5,6 +5,7 @@ namespace TukevastiIlmassaDataCombiner\Console\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use TukevastiIlmassaDataCombiner\Wiki\WmlParser;
 
 class RunCommand extends Command
 {
@@ -13,6 +14,11 @@ class RunCommand extends Command
   }
 
   protected function execute(InputInterface $input, OutputInterface $output) {
-    $output->writeln("test");
+    $rawWiki  = file_get_contents('wiki2.txt');
+    $rawFiles = scandir('.');
+
+    $wikiParser = new WmlParser();
+    $wikiData   = $wikiParser->parse($rawWiki);
+    print_r($wikiData);
   }
 }
