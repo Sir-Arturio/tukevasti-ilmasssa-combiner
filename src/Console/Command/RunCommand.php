0a8892f71107354d10886684f77f078c0bc0f0b8
Sir-Arturio <arto.iijalainen@gmail.com>
2015-04-04 12:01:47 +0300

    Added DirectoryListingParser and tag.php to the RunCommand sequence. Removed obsolete code from tag.php and modified read_tags_and_combine_to_csv() to take in source data as parameters. Reformatted RunCommand to use Symfony2 coding standards.

--- src/Console/Command/RunCommand.php
+++ src/Console/Command/RunCommand.php
@@ -5,20 +5,27 @@ namespace TukevastiIlmassaDataCombiner\Console\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use TukevastiIlmassaDataCombiner\File\DirectoryListingParser;
 use TukevastiIlmassaDataCombiner\Wiki\WmlParser;
 
 class RunCommand extends Command
 {
-  protected function configure() {
-    $this->setName("run");
-  }
+    protected function configure()
+    {
+        $this->setName("run");
+    }
 
-  protected function execute(InputInterface $input, OutputInterface $output) {
-    $rawWiki  = file_get_contents('wiki2.txt');
-    $rawFiles = scandir('.');
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $rawWiki = file_get_contents('wiki2.txt');
+        $rawFiles = scandir('.');
 
-    $wikiParser = new WmlParser();
-    $wikiData   = $wikiParser->parse($rawWiki);
-    print_r($wikiData);
-  }
+        $wikiParser = new WmlParser();
+        $wikiData = $wikiParser->parse($rawWiki);
+
+        $fileParser = new DirectoryListingParser();
+        $files = $fileParser->parse($rawFiles);
+
+        read_tags_and_combine_to_csv($files, $wikiData);
+    }
 }
--- tag.php
+++ tag.php
@@ -1,31 +1,20 @@
 <?php
 
-function read_tags_and_combine_to_csv() {
-  $files = scandir('.');
-  $files = array_filter($files, function ($file) { return preg_match('|.mp3$|', $file); });
+function read_tags_and_combine_to_csv($files, $wikiData) {
 
-  $file_data = array_map("process_file", $files);
-  //$file_dates = array_filter($file_dates, function($val) { return ($val[1] == '00-00-0000');});
-  // print_r($file_dates);
-
-  $matches = read_wiki_content();
-  //print_r(array_pop($matches));
-
-  //echo "FILES: " . count($file_dates) ." WIKI: " . count($matches) ." \n";
-
-  $file_dates = array_map(function ($a) { return $a[1]; }, $file_data);
+  $file_dates = array_map(function ($a) { return $a[1]; }, $files);
   //print_r($file_dates);
 
-  $non_matching_matches = array_filter($matches, function ($a) use($file_dates) { return !in_array($a[1], $file_dates); });
+  $non_matching_matches = array_filter($wikiData, function ($a) use($file_dates) { return !in_array($a[1], $file_dates); });
   print_r($non_matching_matches);
   echo "NON MATCHING COUNT: " . count($non_matching_matches) ." \n";
 
   // COMBINE DATA
   $combined = array();
-  foreach($file_data as $item) {
+  foreach($files as $item) {
     $combined[$item[1]] = $item;
   }
-  foreach($matches as $item) {
+  foreach($wikiData as $item) {
     $combined[$item[1]] = (array_key_exists($item[1], $combined)) ? array_merge($combined[$item[1]], $item) : array_merge(array(null, null), $item);
   }
   ksort($combined);
@@ -48,41 +37,6 @@ function read_tags_and_combine_to_csv() {
   }
   array_walk($combined, function($a) { if(empty($a[0])) echo "FILE " . $a[2] . " MISSING. PREV FILE "; });
 }
-
-function get_date_from_filename($filename) {
-  $exceptions = array('201306010' => '20130610');
-
-  preg_match("/^[0-9]+/", $filename, $matches);
-  $dateStr = (isset($matches)) ? current($matches) : false;
-
-  if(array_key_exists($dateStr, $exceptions)) {
-    echo "CONVERTING DATE FROM: '" . $dateStr . "' ";
-    $dateStr = $exceptions[$dateStr];
-    echo "TO: '". $dateStr ."'\n";
-  }
-
-  $date = DateTime::createFromFormat("Ymd H:i:s", $dateStr . " 00:00:00");
-  if(!is_object($date)) {
-    echo "ERROR CONVERTING '" . $dateStr . "'";
-  }
-  $date = is_object($date) ? $date->format("Y-m-d") : '0000-00-00';
-  return $date;
-  //return DateTime::createFromFormat("Ymd H:i:s", $dateStr . " 00:00:00")->format("Y-m-d");
-}
-
-function process_file($filename) {
-  return array($filename, get_date_from_filename($filename));
-}
-
-
-
-
-//$combined = array_map(function ($a, $b) { $b = is_array($b) ? $b : array(); return array_merge($a, $b); }, $file_dates, $matches);
-//print_r($combined);
-
-//$wiki_dates = array_map(function ($entry) { return is_object($entry[1]) ? $entry[1]->format("Y-m-d") : '0000-00-00'; }, $matches);
-//print_r($wiki_dates);
-//print_r($wiki_dates);
 
 function date_differ($a, $b) {
   $aParts = explode("-", $a);
