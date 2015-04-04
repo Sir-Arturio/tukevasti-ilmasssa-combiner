0a28de7dab4992f84fca3edf9a5ed7b70bdb757d
Sir-Arturio <arto.iijalainen@gmail.com>
2015-04-04 12:18:51 +0300

    Reformatted WmlParser.

--- src/Wiki/WmlParser.php
+++ src/Wiki/WmlParser.php
@@ -6,57 +6,63 @@ use DateTime;
 
 class WmlParser
 {
-  /**
-   * @param $rawData
-   * @return array
-   */
-  public function parse($rawData) {
-    $content = str_replace(array("[[", "]]", "&nbsp;"), '', $rawData);
-    $content = preg_replace('/!\|/m', '||', $content);
-    $matches = preg_split('/\|-|{\|/', $content);
+    /**
+     * @param $rawData
+     * @return array
+     */
+    public function parse($rawData)
+    {
+        $content = str_replace(array("[[", "]]", "&nbsp;"), '', $rawData);
+        $content = preg_replace('/!\|/m', '||', $content);
+        $matches = preg_split('/\|-|{\|/', $content);
 
-    foreach($matches as &$row) {
-      $cells = explode("\n", $row);
-      $cells = array_filter($cells, function ($cell) { return preg_match('/\|\|/', $cell); });
-      $cells = preg_replace('/\|\|\s*/', '', $cells);
-      if(isset($cells[1]) && count($cells) >= 3) {
-        $cells[1] = $this->prepare_date($cells[1]);
-      }
-      $row = $cells;
+        foreach ($matches as &$row) {
+            $cells = explode("\n", $row);
+            $cells = array_filter($cells, function ($cell) { return preg_match('/\|\|/', $cell); });
+            $cells = preg_replace('/\|\|\s*/', '', $cells);
+            if (isset($cells[1]) && count($cells) >= 3) {
+                $cells[1] = $this->prepare_date($cells[1]);
+            }
+            $row = $cells;
+        }
+
+        $matches = array_filter($matches);
+        $matches = array_filter($matches, function($a) { return (is_object($a[1])) ? true : false; });
+        $matches = array_filter($matches, function($a) { return ($a[2] == 'Uusinta edellisestä') ? false : true; });
+        usort($matches, array($this, 'sort_by_date'));
+        $matches = array_map(function($a) { $a[1] = $this->flatten_date($a[1]); return $a; }, $matches);
+
+        return $matches;
     }
 
-    $matches = array_filter($matches);
-    $matches = array_filter($matches, function($a) { return (is_object($a[1])) ? true : false; });
-    $matches = array_filter($matches, function($a) { return ($a[2] == 'Uusinta edellisestä') ? false : true; });
-    usort($matches, array($this, 'sort_by_date'));
-    $matches = array_map(function($a) { $a[1] = $this->flatten_date($a[1]); return $a; }, $matches);
-    return $matches;
-  }
+    protected function sort_by_date($a, $b)
+    {
+        $a = $this->flatten_date($a[1]);
+        $b = $this->flatten_date($b[1]);
 
-  protected function sort_by_date($a, $b) {
-    $a = $this->flatten_date($a[1]);
-    $b = $this->flatten_date($b[1]);
-    return strcmp($a, $b);
-  }
-
-  protected function flatten_date(DateTime $date) {
-    return $date->format("Y-m-d");
-  }
-
-  /**
-   * @param $finnish_date string
-   * @return Date
-   **/
-  protected function prepare_date($finnishDate) {
-    $dateParts = explode(" ", $finnishDate);
-    if(count($dateParts) == 1) {
-      return $finnishDate;
+        return strcmp($a, $b);
     }
-    $finMonths = array('tammikuuta', 'helmikuuta', 'maaliskuuta', 'huhtikuuta', 'toukokuuta', 'kesäkuuta', 'heinäkuuta', 'elokuuta', 'syyskuuta', 'lokakuuta', 'marraskuuta', 'joulukuuta');
-    $dateParts[1] = array_search($dateParts[1], $finMonths) +1;
-    //echo $dateParts[0] . $dateParts[1] . "." . $dateParts[2] . "\n";
 
-    return DateTime::createFromFormat("j.n.Y H:i:s", $dateParts[0] . $dateParts[1] . "." . $dateParts[2] . " 00:00:00");
-  }
+    protected function flatten_date(DateTime $date)
+    {
+        return $date->format("Y-m-d");
+    }
+
+    /**
+     * @param $finnishDate string
+     * @return DateTime
+     */
+    protected function prepare_date($finnishDate)
+    {
+        $dateParts = explode(" ", $finnishDate);
+        if (count($dateParts) == 1) {
+            return $finnishDate;
+        }
+        $finMonths = array('tammikuuta', 'helmikuuta', 'maaliskuuta', 'huhtikuuta', 'toukokuuta', 'kesäkuuta', 'heinäkuuta', 'elokuuta', 'syyskuuta', 'lokakuuta', 'marraskuuta', 'joulukuuta');
+        $dateParts[1] = array_search($dateParts[1], $finMonths) +1;
+        //echo $dateParts[0] . $dateParts[1] . "." . $dateParts[2] . "\n";
+
+        return DateTime::createFromFormat("j.n.Y H:i:s", $dateParts[0] . $dateParts[1] . "." . $dateParts[2] . " 00:00:00");
+    }
 
 } 
