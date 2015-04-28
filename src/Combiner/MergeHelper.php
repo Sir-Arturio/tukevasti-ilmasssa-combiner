569feec6cbdfe7b86d7ae2eae8547fa62edf792c
Sir-Arturio <arto.iijalainen@gmail.com>
2015-04-28 14:39:17 +0300

    Added DocBlock for isAdjacentDate().

--- src/Combiner/MergeHelper.php
+++ src/Combiner/MergeHelper.php
@@ -6,6 +6,13 @@ use DateTime;
 
 class MergeHelper
 {
+    /**
+     * Check if dates are adjacent.
+     *
+     * @param DateTime $date
+     * @param DateTime $date2
+     * @return bool
+     */
     public function isAdjacentDate(DateTime $date, DateTime $date2) {
         $diff = $date->diff($date2);
         return $diff->days == 1;
