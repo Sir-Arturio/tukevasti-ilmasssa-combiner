c7a7f25dfe568b802188f5244c94c626793c5049
Sir-Arturio <arto.iijalainen@gmail.com>
2015-05-01 16:58:15 +0300

    Implemented functionality that loops through the item list and merges the mergeable items with MergeHelper.

--- src/Combiner/WikiFileCombiner.php
+++ src/Combiner/WikiFileCombiner.php
@@ -4,7 +4,14 @@ namespace TukevastiIlmassaDataCombiner\Combiner;
 
 class WikiFileCombiner
 {
-    public function combine($wikiData, $files) {
+    /**
+     * Combines the data from Wiki and Files together into a single element array.
+     *
+     * @param array $wikiData
+     * @param array $files
+     * @return array
+     */
+    public function combine(array $wikiData, array $files) {
         $combined = array();
         foreach ($files as $item) {
             $combined[$item[1]] = $item;
@@ -14,5 +21,53 @@ class WikiFileCombiner
         }
         ksort($combined);
         return $combined;
+    }
+
+    /**
+     * @param array $source
+     * @param MergeHelper $helper
+     * @return array
+     */
+    public function merge(array $source, $helper) {
+        $result = array();
+
+        end($source);
+        $lastKey = key($source);
+
+        $previous = null;
+        foreach($source as $key => $current) {
+            // Do not try to merge if the previous item does not exist or is unmergeable.
+            if($previous == null) {
+                $previous = $current;
+
+                // In case of the last element, the loop will not continue, so the current key has to be saved now.
+                if($key == $lastKey) {
+                    $result[] = $current;
+                }
+                continue;
+            }
+
+            $merged = $helper->mergeItems($previous, $current);
+            // If merge succeeds, save merged result.
+            // The current is already saved in the merged element, so it will not qualify as a next previous item.
+            if($merged !== false) {
+                $result[] = $merged;
+                $previous = null;
+                // Force continue as the last key check would cause the merged element to be saved if it's the last element.
+                continue;
+            }
+            // Merge failed, so the previous has to saved without being merged and the current element qualifies as the next previous element.
+            else {
+                $result[] = $previous;
+                $previous = $current;
+            }
+
+            // In case of the last element, the loop will not continue, so the current key has to be saved now.
+            if($key == $lastKey) {
+                $result[] = $current;
+            }
+        }
+
+        return $result;
     }
 } 
--- tests/Combiner/WikiFileCombinerTest.php
+++ tests/Combiner/WikiFileCombinerTest.php
@@ -2,9 +2,10 @@
 
 namespace TukevastiIlmassaDataCombiner\Combiner;
 
-use PHPUnit_Framework_TestCase;
+use Prophecy\Argument;
+use Prophecy\PhpUnit\ProphecyTestCase;
 
-class WikiFileCombinerTest extends PHPUnit_Framework_TestCase
+class WikiFileCombinerTest extends ProphecyTestCase
 {
     public function testCombinerCombinesWikiAndFileDataToAFormattedResult()
     {
@@ -70,5 +71,159 @@ class WikiFileCombinerTest extends PHPUnit_Framework_TestCase
         $combiner = new WikiFileCombiner();
         $result = $combiner->combine($wiki, $files);
         $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @dataProvider elementArrayProvider
+     */
+    public function testMergeLoopsThroughTheGivenElementArrayAndMergesMergeableItems($items, $expected, $message) {
+        $combiner = new WikiFileCombiner();
+
+        $mergeHelper = $this->prophesize('TukevastiIlmassaDataCombiner\Combiner\MergeHelper');
+        $mergeHelper->mergeItems(Argument::any(), Argument::any())->will(function($args) {
+              $item = $args[0];
+              $item2 = $args[1];
+              if(!isset($item['mergeable']) || !isset($item2['mergeable'])) {
+                  return false;
+              }
+              return array($item[0], $item2[0]);
+          });
+
+        $result = $combiner->merge($items, $mergeHelper->reveal());
+        $this->assertEquals($expected, $result, $message);
+    }
+
+    public function mockMerge() {
+        print_r(get_defined_vars());
+        if(!isset($item['mergeable']) || !isset($item2['mergeable'])) {
+            return false;
+        }
+
+        return array($item[0], $item2[0]);
+    }
+
+    public function elementArrayProvider() {
+        return array(
+            array(
+              array(
+                array('a'),
+                array('b'),
+                array('c', 'mergeable' => true),
+                array('d', 'mergeable' => true),
+                array('e'),
+                array('f'),
+              ),
+              array(
+                array('a'),
+                array('b'),
+                array('c', 'd'),
+                array('e'),
+                array('f'),
+              ),
+              'Mergeable items in the middle of the list are merged.',
+            ),
+
+          array(
+            array(
+              array('a', 'mergeable' => true),
+              array('b', 'mergeable' => true),
+              array('c'),
+              array('d'),
+            ),
+            array(
+              array('a', 'b'),
+              array('c'),
+              array('d'),
+            ),
+            'Mergeable items at the beginning of the list are merged.',
+          ),
+
+          array(
+            array(
+              array('a'),
+              array('b'),
+              array('c', 'mergeable' => true),
+              array('d', 'mergeable' => true),
+            ),
+            array(
+              array('a'),
+              array('b'),
+              array('c', 'd'),
+            ),
+            'Mergeable items at the end of the list are merged.',
+          ),
+
+
+          array(
+            array(
+              array('a'),
+              array('b'),
+              array('c', 'mergeable' => true),
+              array('d', 'mergeable' => true),
+              array('e'),
+            ),
+            array(
+              array('a'),
+              array('b'),
+              array('c', 'd'),
+              array('e'),
+            ),
+            'Mergeable items at the end -1 of the list are merged.',
+          ),
+
+          array(
+            array(
+              array('a'),
+              array('b', 'mergeable' => true),
+              array('c', 'mergeable' => true),
+              array('d'),
+              array('e'),
+            ),
+            array(
+              array('a'),
+              array('b', 'c'),
+              array('d'),
+              array('e'),
+            ),
+            'Mergeable items at the beginning +1 of the list are merged.',
+          ),
+
+          array(
+            array(
+              array('a'),
+              array('b'),
+              array('c'),
+            ),
+            array(
+              array('a'),
+              array('b'),
+              array('c'),
+            ),
+            'Unmergeable items stay tha same.',
+          ),
+
+          array(
+            array(),
+            array(),
+            'An empty list is processed without errors.',
+          ),
+
+          array(
+            array(
+              array('a', 'mergeable' => true),
+              array('b', 'mergeable' => true),
+              array('c', 'mergeable' => true),
+              array('d', 'mergeable' => true),
+              array('e', 'mergeable' => true),
+              array('f', 'mergeable' => true),
+            ),
+            array(
+              array('a', 'b'),
+              array('c', 'd'),
+              array('e', 'f'),
+            ),
+            'A list full of mergeable items merges properly.',
+          ),
+        );
     }
 } 
