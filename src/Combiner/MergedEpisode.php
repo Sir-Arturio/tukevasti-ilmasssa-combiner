07a6bf41442d1e5e507ceaae7e9c947e8c94f437
Sir-Arturio <arto.iijalainen@gmail.com>
2016-03-29 12:36:03 +0300

    Allowed WikiEpisode or FileData to be null in MergedEpisode.

--- src/Combiner/MergedEpisode.php
+++ src/Combiner/MergedEpisode.php
@@ -7,45 +7,45 @@ use TukevastiIlmassaDataCombiner\File\FileData,
 
 class MergedEpisode
 {
-    /** @var FileData $fileData */
+    /** @var FileData|null $fileData */
     protected $fileData;
 
-    /** @var WikiEpisodeInfo $wikiEpisodeInfo */
+    /** @var WikiEpisodeInfo|null $wikiEpisodeInfo */
     protected $wikiEpisodeInfo;
 
     /**
-     * @param FileData $fileData
-     * @param WikiEpisodeInfo $wikiEpisodeInfo
+     * @param FileData|null $fileData
+     * @param WikiEpisodeInfo|null $wikiEpisodeInfo
      */
-    function __construct(FileData $fileData, WikiEpisodeInfo $wikiEpisodeInfo)
+    function __construct($fileData, $wikiEpisodeInfo)
     {
         $this->fileData = $fileData;
         $this->wikiEpisodeInfo = $wikiEpisodeInfo;
     }
 
   /**
-   * @return FileData
+   * @return FileData|null
    */
   public function getFileData() {
     return $this->fileData;
   }
 
   /**
-   * @param FileData $fileData
+   * @param FileData|null $fileData
    */
   public function setFileData($fileData) {
     $this->fileData = $fileData;
   }
 
   /**
-   * @return WikiEpisodeInfo
+   * @return WikiEpisodeInfo|null
    */
   public function getWikiEpisodeInfo() {
     return $this->wikiEpisodeInfo;
   }
 
   /**
-   * @param WikiEpisodeInfo $wikiEpisodeInfo
+   * @param WikiEpisodeInfo|null $wikiEpisodeInfo
    */
   public function setWikiEpisodeInfo($wikiEpisodeInfo) {
     $this->wikiEpisodeInfo = $wikiEpisodeInfo;
